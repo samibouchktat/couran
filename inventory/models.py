78ba9129c57582290d6b14184873e0c7455262a3
Samibouchktat <samibouchktat1@gmail.com>
2025-07-24 15:40:36 +0100

    final

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.utils import timezone
 from django.conf import settings
-from django.contrib.auth import get_user_model
 
 
 
